2b529ed7efd30c5cdaaeea8c39e5255388d70943
komalsavaliya <komalsavaliya85@gmail.com>
2023-11-16 12:13:56 -0600

    wed.writting Hw

---
+++ src/Programme_1_calculate/Main.java
@@ -0,0 +1,27 @@
+package Programme_1_calculate;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Declaring a scanner object
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        do {
+            System.out.println("Please first number :");//Input wizard
+            int a = scanner.nextInt();
+            System.out.println("Please second number :");//Input wizard
+            int b = scanner.nextInt();
+            System.out.println("Please enter one of symbol +.-,*,/ :");//Input wizard
+            char symbol = scanner.next().charAt(0);
+            Calculator calculator = new Calculator();
+            calculator.calculateResult(a, b, symbol);
+            System.out.println("Would you like to do more calculation Please enter “Y” or “N” : ");// Input wizard
+            answer = scanner.next().toLowerCase(Locale.ROOT);
+        }
+        while (answer.startsWith("y"));
+        //Closing a scanner object
+        scanner.close();
+    }
+}
---
+++ src/homework_week_9/Programme_5_ArrayListAtGivenIndex.java
@@ -0,0 +1,23 @@
+package homework_week_9;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Programme_5_ArrayListAtGivenIndex {
+    public static void main(String[] args) {
+        ArrayList<String> fruitList = new ArrayList<>();
+        fruitList.add("APPLE");
+        fruitList.add("BANANA");
+        fruitList.add("GRAPE");
+        fruitList.add("MANGO");
+        fruitList.add("KIWI");
+        fruitList.add("ORANGE");
+        fruitList.add("BLACKBERRY");
+        fruitList.add("STRAWBERRY");
+        fruitList.add("CHERRY");
+        ListIterator<String> iterate = fruitList.listIterator();
+        while (iterate.hasNext()) {
+            System.out.println(iterate.next() + ", ");
+        }
+    }
+}
